a13d3a971c05709c44b4a796115504eeac4e558f
Liz-cloud <61148237+Liz-cloud@users.noreply.github.com>
2021-06-22 21:38:37 +0200

    Update main.c
    changed code

--- 3.5a)/main.c
+++ 3.5a)/main.c
@@ -1,79 +1,71 @@
-
+#define TITLE "DECIMAL TO RADIX-i converter \n"
+#define AUTHOR "Written by: Linda Mafunu \n"
+#define YEAR "Date: 2021 \n"
+#include <stdint.h>
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
-# define TITLE "DECIMAL TO RADIX-i converter"
-# define AUTHOR "Name"
-# define YEAR "2021"
 
-char* Dec2RadixI(int radix, int decimal){
-    char res[]={'0','1','2','3','4','5','6','7','8','9',
-                'A','B','C','D','E','F'};
-    char* ptr={'\0'};
-    int size= (int)log2(decimal);
-    int i = 1;            // counter
-    char result[size];
-    ptr=NULL;
-    while(decimal>0){
-        result[size-i]=res[decimal%radix];
-        ptr=&result[size];
-        ptr-=i;
-        decimal /=radix;
-        i++;
-    }
-//    for(int i =0 ;i<size;i++){
-//        printf("Output at %d is: %c\n",i,result[i]);
-//    }
-    return ptr;
+int convert(int number,int base){
+    if(number == 0 || base==10)
+        return number;
+    return (number % base) + 10*convert(number / base, base);
 }
-int main()
-{
-    printf("*****************************\n");
-    printf(" %s\n",TITLE);
-    printf(" Written by: %s\n",AUTHOR);
-    printf(" Date: %s\n",YEAR);
-    printf("*****************************\n");
-    int decimal_num=0;
-    int radix=0;
-    double logn=0;
-    int divison=0;
-    int remainder=0;
 
-    printf("Enter a decimal number: ");
-    scanf("%d",&decimal_num);
-    printf("The number you have entered is %d\n",decimal_num);
+void Dec2RadixI(int number,int base){
 
-    while(decimal_num>=0){
+    char bases[16] = {'0', '1', '2', '3', '4', '5', '6', '7','8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    int converted[64];
+    int next_digit, index=0;
+
+    /* convert to the indicated base */
+    while (number != 0){
+        converted[index] = number % base;
+        number = number / base;
+        ++index;
+    }
+
+    /* now print the result in reverse order */
+    --index;  /* back up to last entry in the array */
+    for(  ; index>=0; index--){ /* go backward through array */
+        printf("%c", bases[converted[index]]);
+    }
+    printf("\n");
+
+}
+
+int main(void){
+
+    printf("******** \n");
+    printf(TITLE,"%s");
+    printf(AUTHOR,"%s");
+    printf(YEAR, "%s");
+    printf("******** \n");
+
+    //taking in user input
+    int number = 0;
+    while (number >= 0){
+        printf("Enter a decimal number: ");
+        scanf("%d",&number);
+        if (number < 0 ){
+            printf("EXIT \n");
+            break;
+        }
+        printf("The number you have entered is: %d\n",number);
+        //taking in the radix
+        int radix;
         printf("Enter a radix for the converter between 2 and 16: ");
         scanf("%d",&radix);
-        printf("The radix you have entered is %d\n",radix);
+        printf("The radix you have entered is %d\n ",radix);
 
-        logn= log2(decimal_num);
-        printf("The log2 of the number is %.2lf\n",logn);
+        printf("The log2 of the number is ");
+        printf("%.2f \n",log2(number));
 
-        divison=decimal_num/radix;
-        printf("The integer result of the number divide by %d is %d\n",radix,divison);
-
-        remainder=decimal_num%radix;
-        printf("The remainder is %d\n",remainder);
-        char output[]={'\0'};
-        strcat(output,Dec2RadixI(radix,decimal_num));
-
-//        for(int i =0 ;i<(sizeof (output)/(sizeof (output[0])));i++){
-//            printf("Output at %d is: %s\n",i,output[i]);
-//        }
-        printf("The radix-%d value is %s\n",radix,output);
-
-
-        printf("Enter a decimal number: ");
-        scanf("%d",&decimal_num);
-        output[0]=0;
+        //parth
+        printf("The integer result of the number divided by %d is %d \n",number,number/radix);
+        printf("The remainder is %d \n",number%radix);
+        printf("The radix-%d value is ",radix);
+        Dec2RadixI(number,radix);
     }
-    printf("EXIT");
 
-    return 0;
 }
-
